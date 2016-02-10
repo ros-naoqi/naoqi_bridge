c18cd52462c2038ccee3b390e85a14d6bc0c750d
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2016-02-10 12:26:06 +0900

    rewrite the program by using NaoqiNode and added set/getExternalCollisionProtectionEnabled service

--- naoqi_apps/nodes/naoqi_externalCollisionAvoidance.py
+++ naoqi_apps/nodes/naoqi_externalCollisionAvoidance.py
@@ -1,169 +1,151 @@
 #!/usr/bin/env python
 
-#
-# ROS node to set Pepper's orthogonal security distance, tangential security 
-# distance and read move faied information
-# This code is currently compatible to NaoQI version 2.3
-#
-# Copyright 2015 Kanae Kochigami, The University of Tokyo
-# http://wiki.ros.org/pepper
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#    # Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#    # Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#    # Neither the name of the University of Freiburg nor the names of its
-#       contributors may be used to endorse or promote products derived from
-#       this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-
+#                                                                             
+#  Copyright 2015 Aldebaran                                                   
+#                                                                             
+#  Licensed under the Apache License, Version 2.0 (the "License");            
+#  you may not use this file except in compliance with the License.           
+#  You may obtain a copy of the License at                                    
+#                                                                             
+#      http://www.apache.org/licenses/LICENSE-2.0                             
+#                                                                             
+#  Unless required by applicable law or agreed to in writing, software        
+#  distributed under the License is distributed on an "AS IS" BASIS,          
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+#  See the License for the specific language governing permissions and        
+#  limitations under the License.                                             
+#                                                                             
+# 
 import rospy
-import sys
-import naoqi
 from naoqi_driver.naoqi_node import NaoqiNode
-from naoqi import ( ALModule, ALBroker, ALProxy )
 from std_msgs.msg import String 
 from std_srvs.srv import (
     EmptyResponse,
     Empty)
-#from naoqi_bridge_msgs.srv import SetArmsEnabled
-from naoqi_bridge_msgs.srv import ( TangentialSecurityDistance, OrthogonalSecurityDistance )
+from naoqi_bridge_msgs.srv import ( 
+    TangentialSecurityDistanceResponse,
+    TangentialSecurityDistance, 
+    OrthogonalSecurityDistanceResponse,
+    OrthogonalSecurityDistance,
+    SetExternalCollisionProtectionEnabledResponse,
+    SetExternalCollisionProtectionEnabled,
+    GetExternalCollisionProtectionEnabledResponse,
+    GetExternalCollisionProtectionEnabled,)
 
-#
-# Notes:
-# - A port number > 0 for the module must be specified.
-#   If port 0 is used, a free port will be assigned automatically,
-#   but naoqi is unable to pick up the assigned port number, leaving
-#   the module unable to communicate with naoqi (1.10.52).
-#
-# - Callback functions _must_ have a docstring, otherwise they won't get bound.
-#
-# - Shutting down the broker manually will result in a deadlock,
-#   not shutting down the broker will sometimes result in an exception
-#   when the script ends (1.10.52).
-#
-
-class NaoqiExternalCollisionAvoidance(ALModule):
-    "Sends callbacks for bumper failed information to ROS"
-    def __init__(self, moduleName):
-        # get connection from command line:
-        from optparse import OptionParser
-
-        parser = OptionParser()
-        parser.add_option("--ip", dest="ip", default="",
-                          help="IP/hostname of broker. Default is system's default IP address.", metavar="IP")
-        parser.add_option("--port", dest="port", default=0,
-                          help="IP/hostname of broker. Default is automatic port.", metavar="PORT")
-        parser.add_option("--pip", dest="pip", default="127.0.0.1",
-                          help="IP/hostname of parent broker. Default is 127.0.0.1.", metavar="IP")
-        parser.add_option("--pport", dest="pport", default=9559,
-                          help="port of parent broker. Default is 9559.", metavar="PORT")
-
-        (options, args) = parser.parse_args()
-        self.ip = options.ip
-        self.port = int(options.port)
-        self.pip = options.pip
-        self.pport = int(options.pport)
-        self.moduleName = moduleName
-
-        self.init_almodule()
-
-        # ROS initialization:
-        rospy.init_node('naoqi_externalCollisionAvoidance')
-
+class NaoqiExternalCollisionAvoidance(NaoqiNode):
+    def __init__(self):
+        NaoqiNode.__init__(self, 'naoqi_externalCollisionAvoidance')
+        self.connectNaoQi()
+        
         # init. messages:
         self.moveFailed = String()
-        
-        self.setOrthogonalSecurityDistanceSrv = rospy.Service("naoqi_externalCollisionAvoidance/orthogonal_distance", OrthogonalSecurityDistance, self.handleOrthogonalSecurityDistanceSrv)
-        self.setTangentialSecurityDistanceSrv = rospy.Service("naoqi_externalCollisionAvoidance/tangential_distance", TangentialSecurityDistance, self.handleTangentialSecurityDistanceSrv)
-        self.moveFailedPub = rospy.Publisher("move_failed", String, queue_size=10)
-        
-        self.subscribe()
+        self.previousMoveFailedInfomation = ""
+        self.setOrthogonalSecurityDistanceSrv = rospy.Service("set_orthogonal_distance", OrthogonalSecurityDistance, self.handleSetOrthogonalSecurityDistanceSrv)
+        self.setTangentialSecurityDistanceSrv = rospy.Service("set_tangential_distance", TangentialSecurityDistance, self.handleSetTangentialSecurityDistanceSrv)
+        self.getMoveFailedInformationSrv = rospy.Service("move_failed_information", Empty, self.handleMoveFailedInformationSrv)
+        self.setOrthogonalSecurityDistanceSrv = rospy.Service("get_orthogonal_distance", Empty, self.handleGetOrthogonalSecurityDistanceSrv)
+        self.setTangentialSecurityDistanceSrv = rospy.Service("get_tangential_distance", Empty, self.handleGetTangentialSecurityDistanceSrv)
+        self.setExternalCollisionProtectionEnabledSrv = rospy.Service("set_external_collision_protection_status", SetExternalCollisionProtectionEnabled, self.handleSetExternalCollisionProtectionEnabled)
+        self.getExternalCollisionProtectionEnabledSrv = rospy.Service("get_external_collision_protection_status", GetExternalCollisionProtectionEnabled, self.handleGetExternalCollisionProtectionEnabled)
+        rospy.loginfo("naoqi_externalCollisionAvoidance initialized")
 
-        rospy.loginfo("nao_externalCollisionAvoidance initialized")
-
-    def init_almodule(self):
-        # before we can instantiate an ALModule, an ALBroker has to be created
+    def connectNaoQi(self):
         rospy.loginfo("Connecting to NaoQi at %s:%d", self.pip, self.pport)
-        try:
-            self.broker = ALBroker("%sBroker" % self.moduleName, self.ip, self.port, self.pip, self.pport)
-        except RuntimeError,e:
-            print("Could not connect to NaoQi's main broker")
+        self.memProxy = self.get_proxy("ALMemory")
+        self.motionProxy = self.get_proxy("ALMotion")
+        if self.memProxy is None or self.motionProxy is None:
             exit(1)
-        ALModule.__init__(self, self.moduleName)
-
-        self.memProxy = ALProxy("ALMemory", self.pip, self.pport)
-        # TODO: check self.memProxy.version() for > 1.6
-        if self.memProxy is None:
-            rospy.logerror("Could not get a proxy to ALMemory on %s:%d", self.pip, self.pport)
-            exit(1)
-        self.motionProxy = ALProxy("ALMotion", self.pip, self.pport)
-
-
-    def shutdown(self):
-        self.unsubscribe()
-        # Shutting down broker seems to be not necessary any more
-        # try:
-        #     self.broker.shutdown()
-        # except RuntimeError,e:
-        #     rospy.logwarn("Could not shut down Python Broker: %s", e)
-
-
-    def subscribe(self):
-        self.memProxy.subscribeToEvent("ALMotion/MoveFailed", self.moduleName, "onMoveFailed")
         
+    def handleSetOrthogonalSecurityDistanceSrv(self, req):
+        try:
+            if (req.orthogonal_distance > 0.001 or req.orthogonal_distance == 0.001):
+                self.motionProxy.setOrthogonalSecurityDistance(req.orthogonal_distance.data)
+                rospy.loginfo("Orthogonal security distance is set to " + str(req.orthogonal_distance.data))
+                return OrthogonalSecurityDistanceResponse()
+            else:
+                rospy.loginfo("Orthogonal security distance should be 0.001, or more than 0.001")
+                return OrthogonalSecurityDistanceResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+        
+    def handleSetTangentialSecurityDistanceSrv(self, req):
+        try:
+            if (req.tangential_distance > 0.001 or req.tangential_distance == 0.001):
+                self.motionProxy.setTangentialSecurityDistance(req.tangential_distance.data)
+                rospy.loginfo("Tangential security distance is set to " + str(req.tangential_distance.data))
+                return TangentialSecurityDistanceResponse()
+            else:
+                rospy.loginfo("Tangential distance should be 0.001, or more than 0.001")
+                return TangentialSecurityDistanceResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
+        
+    def handleGetOrthogonalSecurityDistanceSrv(self, req):
+        try:
+            rospy.loginfo("Current Orthogonal Security Distance: " + str(self.motionProxy.getOrthogonalSecurityDistance()))
+            return EmptyResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
 
-    def unsubscribe(self):
-        self.memProxy.unsubscribeToEvent("ALMotion/MoveFailed", self.moduleName)
+    def handleGetTangentialSecurityDistanceSrv(self, req):
+        try:
+            rospy.loginfo("Current Tangential Security Distance: " + str(self.motionProxy.getTangentialSecurityDistance()))
+            return EmptyResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
 
-    def handleOrthogonalSecurityDistanceSrv(self, req):
-        if (req.orthogonal_distance < 0.001 or req.orthogonal_distance == 0.001):
-            return EmptyResponse
-        self.motionProxy.setOrthogonalSecurityDistance(req.orthogonal_distance.data)
-        return EmptyResponse
+    def handleMoveFailedInformationSrv(self, req):
+        if self.memProxy.getData("ALMotion/MoveFailed") != self.previousMoveFailedInfomation:
+            moveFailedData = self.memProxy.getData("ALMotion/MoveFailed")
+            self.previousMoveFailedInformation = moveFailedData
+            moveFailedMessage = "I CANNOT MOVE\n Cause of move failed: " + moveFailedData[0]
+            if moveFailedData[1] == 0:
+                moveFailedMessage  += ", Status: move not started" 
+            if moveFailedData[1] == 1:
+                moveFailedMessage += ", Status: move started but stopped" 
+            moveFailedMessage += ", Obstacle position: "+ str(moveFailedData[2])
+            rospy.loginfo(moveFailedMessage)
+            return EmptyResponse()
 
-    def handleTangentialSecurityDistanceSrv(self, req):
-        if (req.tangential_distance < 0.001 or req.tangential_distance == 0.001):
-            return EmptyResponse
-        self.motionProxy.setTangentialSecurityDistance(req.tangential_distance.data)
-        return EmptyResponse
+    def handleSetExternalCollisionProtectionEnabled(self, req):
+        try:
+            self.motionProxy.setExternalCollisionProtectionEnabled(req.name.data, req.status)
+            if self.motionProxy.getExternalCollisionProtectionEnabled(req.name.data)== True: 
+                rospy.loginfo("Current External Collision Protection Status of " + req.name.data + ": Enabled")
+            else:
+                rospy.loginfo("Current External Collision Protection Status of " + req.name.data + ": Disabled")
+            if (req.name.data == "All" or req.name.data == "Move") and req.status == False:
+                rospy.loginfo("CAUTION: YOU DEACTIVATED SAFETY REFLEXES. BE CAREFUL TO THE SAFETY OF THE ROBOT AND PEOPLE.")
+            return SetExternalCollisionProtectionEnabledResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
 
-    def onMoveFailed(self, strVarName, value, strMessage):
-        "Called when ALMotion/MoveFailed event is raised in ALMemory"
-        publish_message = "Cause of move failed: " + value[0]
-        if value[1] == 0:
-            publish_message += ", Status: move not started" 
-        if value[1] == 1:
-            publish_message += ", Status: move started but stopped" 
-    
-        publish_message += ", Obstacle position: "+ str(value[2])
-        self.moveFailed.data = publish_message;
-        self.moveFailedPub.publish(self.moveFailed)
-        rospy.logdebug("move failed: name=%s, value=%s, message=%s.", strVarName, value, strMessage);
+    def handleGetExternalCollisionProtectionEnabled(self, req):
+        try:
+            if self.motionProxy.getExternalCollisionProtectionEnabled(req.name.data)== True: 
+                rospy.loginfo("Current External Collision Protection Status of " + req.name.data + ": Enabled")
+            else:
+                rospy.loginfo("Current External Collision Protection Status of " + req.name.data + ": Disabled")
+            return GetExternalCollisionProtectionEnabledResponse()
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
 
+    def run(self):
+        while self.is_looping():
+            try:
+                pass
+            except RuntimeError, e:
+                print "Error accessing ALMemory and ALMotion, exiting...\n"
+                print e
+                rospy.signal_shutdown("No NaoQI available anymore")
 
 if __name__ == '__main__':
-    ROSNaoqiExternalCollisionAvoidanceModule = NaoqiExternalCollisionAvoidance("ROSNaoqiExternalCollisionAvoidanceModule")
-
+    externalCollisionAvoidance = NaoqiExternalCollisionAvoidance()
+    externalCollisionAvoidance.start()
     rospy.spin()
-
-    rospy.loginfo("Stopping naoqi_externalCollisionAvoidance ...")
-    ROSNaoqiExternalCollisionAvoidanceModule.shutdown();
-    rospy.loginfo("naoqi_externalCollisionAvoidance stopped.")
-    exit(0)
