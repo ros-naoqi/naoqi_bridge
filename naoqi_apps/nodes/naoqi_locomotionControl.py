b4ed5fb94d3ab0a012fa94a5f535bbe56c49919f
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2016-10-07 12:53:46 +0900

    add service for getMoveConfig

--- naoqi_apps/nodes/naoqi_locomotionControl.py
+++ naoqi_apps/nodes/naoqi_locomotionControl.py
@@ -22,6 +22,8 @@ from std_srvs.srv import (
     EmptyResponse,
     Empty)
 from naoqi_bridge_msgs.srv import ( 
+    GetMoveConfigResponse,
+    GetMoveConfig,
     MoveIsActiveResponse,
     MoveIsActive,
     GetRobotPositionResponse,
@@ -40,6 +42,7 @@ class NaoqiLocomotionControl(NaoqiNode):
         NaoqiNode.__init__(self, 'naoqi_motion')
         self.connectNaoQi()
 
+        self.moveConfigSrv = rospy.Service("get_move_config", GetMoveConfig, self.handleGetMoveConfig)
         self.moveInitSrv = rospy.Service("move_init", Empty, self.handleMoveInit)
         self.waitUnitilMoveIsFinishedSrv = rospy.Service("wait_until_move_is_finished", Empty, self.handleWaitUntilMoveIsFinished)
         self.moveIsActiveSrv = rospy.Service("move_is_active", MoveIsActive, self.handleMoveIsActive)
@@ -57,6 +60,28 @@ class NaoqiLocomotionControl(NaoqiNode):
         self.motionProxy = self.get_proxy("ALMotion")
         if self.memProxy is None or self.motionProxy is None:
             exit(1)
+
+    def handleGetMoveConfig(self, req):
+        try:
+            res = GetMoveConfigResponse()
+            if req.move_config_name.data == 0:
+                config_name = "Max"
+            if req.move_config_name.data == 1:
+                config_name = "Min"
+            if req.move_config_name.data == 2:
+                config_name = "Default"
+            result = self.motionProxy.getMoveConfig(config_name)
+            res.move_config.max_vel_xy = (result[0])[1]
+            res.move_config.max_vel_theta = (result[1])[1]
+            res.move_config.max_acc_xy = (result[2])[1]
+            res.move_config.max_acc_theta = (result[3])[1]
+            res.move_config.max_jerk_xy = (result[4])[1]
+            res.move_config.max_jerk_theta = (result[5])[1]
+            res.move_config.torso_wy = (result[6])[1]
+            return res
+        except RuntimeError, e:
+            rospy.logerr("Exception caught:\n%s", e)
+            return None
             
     def handleMoveInit(self, req):
         try:
