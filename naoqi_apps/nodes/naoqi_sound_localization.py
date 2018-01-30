998ec554509f597b0c62671d297d80f9484c0436
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-01-30 10:55:34 +0900

    replace using msgs

--- naoqi_apps/nodes/naoqi_sound_localization.py
+++ naoqi_apps/nodes/naoqi_sound_localization.py
@@ -19,13 +19,14 @@ import rospy
 from naoqi_driver.naoqi_node import NaoqiNode
 from std_srvs.srv import(
     EmptyResponse,
-    Empty)
+    Empty,
+    SetBoolResponse,
+    SetBool
+)
 from naoqi_bridge_msgs.msg import SoundLocated
 from naoqi_bridge_msgs.srv import(
-    SetEnergyComputationResponse,
-    SetEnergyComputation,
-    SetSensitivityResponse,
-    SetSensitivity)
+    SetFloatResponse,
+    SetFloat)
 
 class NaoqiSoundLocalization (NaoqiNode):
     def __init__(self):
@@ -36,15 +37,19 @@ class NaoqiSoundLocalization (NaoqiNode):
         self.soundLocalizationPub = rospy.Publisher("soundLocated", SoundLocated, queue_size=10)
         self.subscribeSoundLocalizationSrv = rospy.Service("start_sound_localization", Empty, self.handleSubscribeSoundLocalization)
         self.unsubscribeSoundLocalizationSrv = rospy.Service("stop_sound_localization", Empty, self.handleUnsubscribeSoundLocalization)
-        self.setEnergyComputationSrv = rospy.Service("set_energy_computation", SetEnergyComputation, self.handleSetEnergyComputation)
-        self.setSensitivitySrv = rospy.Service("set_sensitivity", SetSensitivity, self.handleSetSensitivity)
+        self.setEnergyComputationSrv = rospy.Service("set_energy_computation", SetBool, self.handleSetEnergyComputation)
+        self.setSensitivitySrv = rospy.Service("set_sensitivity", SetFloat, self.handleSetSensitivity)
         rospy.loginfo("naoqi_soundLocalization is initialized")
        
     def connectNaoQi(self):
         rospy.loginfo("Connecting to NaoQi at %s:%d", self.pip, self.pport)
         self.memProxy = self.get_proxy("ALMemory")
         self.soundLocalizationProxy = self.get_proxy("ALSoundLocalization")
-        if self.memProxy is None or self.soundLocalizationProxy is None:
+        if self.memProxy is None:
+            rospy.logerr("Could not get a proxy to ALMemory")
+            exit(1)
+        if self.soundLocalizationProxy is None:
+            rospy.logerr("Could not get a proxy to ALSoundLocalization")
             exit(1)
     
     def handleSubscribeSoundLocalization(self, req):
@@ -65,8 +70,8 @@ class NaoqiSoundLocalization (NaoqiNode):
             
     def handleSetEnergyComputation(self, req):
         try:
-            res = SetEnergyComputationResponse()
-            self.soundLocalizationProxy.setParameter("EnergyComputation", req.status)
+            res = SetBoolResponse()
+            self.soundLocalizationProxy.setParameter("EnergyComputation", req.data)
             res.success = True
             return res
         except RuntimeError, e:
@@ -76,8 +81,8 @@ class NaoqiSoundLocalization (NaoqiNode):
             
     def handleSetSensitivity(self, req):
         try:
-            res = SetSensitivityResponse()
-            self.soundLocalizationProxy.setParameter("Sensitivity", req.sensitivity)
+            res = SetFloatResponse()
+            self.soundLocalizationProxy.setParameter("Sensitivity", req.data)
             res.success = True
             return res
         except RuntimeError, e:
@@ -88,29 +93,31 @@ class NaoqiSoundLocalization (NaoqiNode):
     def run(self):
         while self.is_looping():
             try:
-                r = rospy.Rate(5)
-                sound = self.memProxy.getData("ALSoundLocalization/SoundLocated")
-                if sound is not None and (len(sound)) > 0 and sound != self.pre_sound_localization:
-                    self.soundLocalization.header.stamp = rospy.get_rostime()
-                    self.soundLocalization.azimuth = sound[1][0]
-                    self.soundLocalization.elevation = sound[1][1]
-                    self.soundLocalization.confidence = sound[1][2]
-                    self.soundLocalization.energy = sound[1][3] 
-                    self.soundLocalization.head_position_frame_torso.linear.x = sound[2][0]
-                    self.soundLocalization.head_position_frame_torso.linear.y = sound[2][1]
-                    self.soundLocalization.head_position_frame_torso.linear.z = sound[2][2]
-                    self.soundLocalization.head_position_frame_torso.angular.x = sound[2][3]
-                    self.soundLocalization.head_position_frame_torso.angular.y = sound[2][4]
-                    self.soundLocalization.head_position_frame_torso.angular.z = sound[2][5]
-                    self.soundLocalization.head_position_frame_robot.linear.x = sound[3][0]
-                    self.soundLocalization.head_position_frame_robot.linear.y = sound[3][1]
-                    self.soundLocalization.head_position_frame_robot.linear.z = sound[3][2]
-                    self.soundLocalization.head_position_frame_robot.angular.x = sound[3][3]
-                    self.soundLocalization.head_position_frame_robot.angular.y = sound[3][4]
-                    self.soundLocalization.head_position_frame_robot.angular.z = sound[3][5]
+                if self.soundLocalizationPub.get_num_connections() > 0:
+                    sound = self.memProxy.getData("ALSoundLocalization/SoundLocated")
+                    if sound is not None and (len(sound)) > 0 and sound != self.pre_sound_localization:
+                        self.soundLocalization.header.stamp = rospy.get_rostime()
+                        self.soundLocalization.azimuth = sound[1][0]
+                        self.soundLocalization.elevation = sound[1][1]
+                        self.soundLocalization.confidence = sound[1][2]
+                        if len(sound[1]) == 4:
+                            self.soundLocalization.energy = sound[1][3]
+                        self.soundLocalization.head_position_frame_torso.linear.x = sound[2][0]
+                        self.soundLocalization.head_position_frame_torso.linear.y = sound[2][1]
+                        self.soundLocalization.head_position_frame_torso.linear.z = sound[2][2]
+                        self.soundLocalization.head_position_frame_torso.angular.x = sound[2][3]
+                        self.soundLocalization.head_position_frame_torso.angular.y = sound[2][4]
+                        self.soundLocalization.head_position_frame_torso.angular.z = sound[2][5]
+                        if len(sound) == 4:
+                            self.soundLocalization.head_position_frame_robot.linear.x = sound[3][0]
+                            self.soundLocalization.head_position_frame_robot.linear.y = sound[3][1]
+                            self.soundLocalization.head_position_frame_robot.linear.z = sound[3][2]
+                            self.soundLocalization.head_position_frame_robot.angular.x = sound[3][3]
+                            self.soundLocalization.head_position_frame_robot.angular.y = sound[3][4]
+                            self.soundLocalization.head_position_frame_robot.angular.z = sound[3][5]
 
-                    self.soundLocalizationPub.publish(self.soundLocalization)
-                    self.pre_sound_localization = sound
+                        self.soundLocalizationPub.publish(self.soundLocalization)
+                        self.pre_sound_localization = sound
                     
             except RuntimeError, e:
                 print "Error accessing ALMemory, exiting...\n"
