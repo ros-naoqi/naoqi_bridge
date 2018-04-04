4bf41eaaeeb2b0e4a4fad29232bcb8b58dfba3d9
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-04-04 16:26:34 +0900

    bugfix: fix file type

--- naoqi_apps/nodes/naoqi_tablet.py
+++ naoqi_apps/nodes/naoqi_tablet.py
@@ -53,7 +53,7 @@ class NaoqiTablet(NaoqiNode):
                
             else:
                 rospy.loginfo("Please confirm the file name and " + str(req.data) + " really exists under /home/nao/.local/share/PackageManager/apps/" + self.image_folder + "html.")
-                res.success.data = False
+                res.success = False
             return res
         except RuntimeError, e:
             rospy.logerr("Exception caught:\n%s", e)
