357ac6806eb8465d5ee89349a4594e00640d6125
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-12-28 17:25:56 +0900

    delete check subscribing connection

--- naoqi_apps/nodes/naoqi_speech.py
+++ naoqi_apps/nodes/naoqi_speech.py
@@ -9,26 +9,6 @@ status: "enqueued", "started", "thrown", "stopped", "done"
 [flow of status]
 (enqueued) => (started) => (thrown/ stopped/ done)
 '''
-# Note
-'''
-At first when launching this, 
-
-rostopic echo /speech_status 
-data: "done"
-
-will appear even though you send any speech topic to a robot.
-It is because the old data remains inside NAOqi API.
-So, we should wait for a while before using /speech_status topic. 
-
-kanae@kanae-ThinkPad-T480s:~/catkin_ws/src/naoqi_bridge/naoqi_apps/nodes$ rostic echo /speech_status 
-data: "done" ## this is old data
----
-data: "started" ## I send speech topic
----
-data: "done"
----
-
-'''
 
 import rospy
 from naoqi_driver.naoqi_node import NaoqiNode
@@ -55,20 +35,19 @@ class NaoqiSpeech(NaoqiNode):
     def run(self):
         while self.is_looping():
             try:
-                if self.speechStatusPub.get_num_connections() > 0:
-                    test = self.memProxy.getData("ALTextToSpeech/Status")
-                    if self.speech_task != test[0]:
-                        self.speech_task = test[0]
-                        self.speech_status = test[1]
-                        tmp = String()
-                        tmp.data = self.speech_status
-                        self.speechStatusPub.publish(tmp)
-                    # self.speech_task == test[0]:
-                    elif self.speech_status != test[1]:
-                        self.speech_status = test[1]
-                        tmp = String()
-                        tmp.data = self.speech_status
-                        self.speechStatusPub.publish(tmp)
+                test = self.memProxy.getData("ALTextToSpeech/Status")
+                if self.speech_task != test[0]:
+                    self.speech_task = test[0]
+                    self.speech_status = test[1]
+                    tmp = String()
+                    tmp.data = self.speech_status
+                    self.speechStatusPub.publish(tmp)
+                # self.speech_task == test[0]:
+                elif self.speech_status != test[1]:
+                    self.speech_status = test[1]
+                    tmp = String()
+                    tmp.data = self.speech_status
+                    self.speechStatusPub.publish(tmp)
             except RuntimeError, e:
                 pass
             self.rate.sleep()
